a144a20e29301535edc5e8d91fb718aaaa54063e
sabbirHossain <sabbirshnt@gmail.com>
2025-04-22 17:38:49 +0600

    done

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import transparent from "../img/transparent.gif";
 import logo from "../img/skyexchange/logo.png";
+import telegram from "../img/telegram.png";
 
-export default { transparent, logo };
+export default { transparent, logo, telegram };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,12 +2,14 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/shared/Header/Header";
 import Footer from "../components/shared/Footer/Footer";
 import Banner from "../components/shared/Banner/Banner";
+import WhatsApp from "../components/shared/WhatsApp";
 
 const MainLayout = () => {
   const showBanner = ["/"];
   const location = useLocation();
   return (
     <div className="ui-mobile-viewport ui-overlay-a">
+      <WhatsApp />
       <div data-role="page" className="ui-page ui-page-theme-a ui-page-active">
         <Header />
         <Footer />
